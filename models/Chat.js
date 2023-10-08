1e609d2123ad6c030e47c245373371e8f4274529
AyushC24 <97463208+AyushC24@users.noreply.github.com>
2023-10-08 23:31:02 +0530

    Added chat and message model

--- models/Chat.js
+++ models/Chat.js
@@ -1,7 +1,24 @@
 const mongoose=require("mongoose");
 var Schema=mongoose.Schema;
 var chatSchema=new Schema({
-
+    User:[{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    }],
+    ChatName:{
+        type:String,
+        required:true
+    },
+    isGroupChat: {
+        type:Boolean,
+        required:true
+    },
+    LatestMessages:[
+        {
+            type:mongoose.Types.ObjectId,
+            ref:"Messsage"
+        }
+    ]
 
 });
 module.exports=mongoose.model("Chat",chatSchema);
--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,16 @@
 const mongoose=require("mongoose");
 var Schema=mongoose.Schema;
 var messageSchema=new Schema({
-
+    Sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    Content:{
+        type:String
+    },
+    Chat:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Chat"
+    }
 });
 module.exports=mongoose.model("Message",messageSchema);
